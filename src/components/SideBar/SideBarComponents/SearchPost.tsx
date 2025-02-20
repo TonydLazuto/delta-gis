ee827bf56d9bca73f5676b80a4ba34560d63bd1b
Anthony <2785518+TonydLazuto@users.noreply.github.com>
2025-02-21 00:23:19 +0100

    Enhance SearchPost component with placeholders and blur effect; update animations in searchPost.scss

--- src/components/SideBar/SideBarComponents/SearchPost.tsx
+++ src/components/SideBar/SideBarComponents/SearchPost.tsx
@@ -2,11 +2,20 @@ import { TextField } from '@mui/material'
 // import { useState } from 'react'
 import '../../../styles/subSideMenu.scss';
 import '../../../styles/searchPost.scss';
+import { useState } from 'react';
+import '../../../styles/postCard.scss';
 
 const SearchPost = () => {
   // const [searchSource, setSearchSource] = useState('SIG') // SIG ou DANY
   // const [searchNature, setSearchNature] = useState('Poste') // Poste ou Artere
+  const [gdoPoste, setGdoPoste] = useState('PlaceHolder')
+  const [artere1, setArtere1] = useState('PlaceHolder')
+  const [artere2, setArtere2] = useState('PlaceHolder')
+  const [fonction, setFonction] = useState('PlaceHolder')
+  const [adresse, setAdresse] = useState('PlaceHolder')
+  const [date_de_MEX, setDate_de_MEX] = useState('PlaceHolder')
 
+  const [blur, setBlur] = useState(true)
   return (
     <div className='searchPostContainer'>
       <div className='subSideSearch fade-in-top-normal'>
@@ -24,8 +33,37 @@ const SearchPost = () => {
           }}
         />
       </div>
-      <div className='fade-in-top-normal-delay'>
-        <h3 className=''>TODO: card ou apparait les placeholder</h3>
+      <div className={`searchPostCardContainer fade-in-top-normal-delay ${blur ? 'blur' : ''}`}>
+        <h3>
+          <span>Poste HTA/BT</span>
+        </h3>
+        <div className="searchPostCard">
+          <div className="searchPostField">
+            <h4>NomDuPoste</h4>
+            <h4>{gdoPoste}</h4>
+          </div>
+          <div className="searchPostField">
+            <h5>Adresse</h5>
+            <div>{adresse}</div>
+          </div>
+          <div className="searchPostField">
+            <h5>Arteres</h5>
+            <div>
+              {artere1}
+              {artere2 && <>, {artere2}</>}
+            </div>
+          </div>
+          <div className="searchPostField">
+            <h5>Fonction</h5>
+            <div>{fonction}</div>
+          </div>
+          {date_de_MEX && (
+            <div className="searchPostField">
+              <h5>Date de MEX</h5>
+              <div>{date_de_MEX}</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
